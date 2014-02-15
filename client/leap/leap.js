4375c8757534df9c5944ff35bc7b759d21589420
marielb <marielbartolome@gmail.com>
2014-02-15 11:43:57 -0500

    leap motion controls

--- client/leap/leap.js
+++ client/leap/leap.js
@@ -1,30 +1,55 @@
 bopit = angular.module('bopitApp')
 .controller("LeapCtrl", function($scope) {
-    var c, cHeight, cWidth, colors, leap, _ref;
-    $scope.numFingers = 0;
     $scope.leap = leap = new Leap.Controller();
-    colors = ["#ac4142", "#d26445", "#90a959", "#75b5aa", "#aa759f"];
-    _ref = (function() {
-        var canvas;
-        canvas = document.getElementById("canvas");
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-        return [canvas.getContext('2d'), canvas.height, canvas.width];
-    })(), c = _ref[0], cHeight = _ref[1], cWidth = _ref[2];
-    return leap.on('frame', function(frame) {
-        return $scope.$apply(function() {
-            var f, i, x, y, z, _i, _len, _ref1, _ref2, _results;
-            c.clearRect(0, 0, cWidth, cHeight);
-            $scope.numFingers = frame.fingers.length;
-            _ref1 = frame.fingers;
-            _results = [];
-            for (i = _i = 0, _len = _ref1.length; _i < _len; i = ++_i) {
-                f = _ref1[i];
-                _ref2 = f.tipPosition, x = _ref2[0], y = _ref2[1], z = _ref2[2];
-                c.fillStyle = colors[i];
-                _results.push(c.fillRect(x + (cWidth / 2), cHeight - y, z, z));
+    // var c, cHeight, cWidth, colors, leap, _ref;
+    // $scope.numFingers = 0;
+    // colors = ["#ac4142", "#d26445", "#90a959", "#75b5aa", "#aa759f"];
+    // _ref = (function() {
+    //     var canvas;
+    //     canvas = document.getElementById("canvas");
+    //     canvas.width = canvas.clientWidth;
+    //     canvas.height = canvas.clientHeight;
+    //     return [canvas.getContext('2d'), canvas.height, canvas.width];
+    // })(), c = _ref[0], cHeight = _ref[1], cWidth = _ref[2];
+
+    // return leap.on('frame', function(frame) {
+    //     return $scope.$apply(function() {
+    //         var f, i, x, y, z, _i, _len, _ref1, _ref2, _results;
+    //         c.clearRect(0, 0, cWidth, cHeight);
+    //         $scope.numFingers = frame.fingers.length;
+    //         _ref1 = frame.fingers;
+    //         _results = [];
+    //         for (i = _i = 0, _len = _ref1.length; _i < _len; i = ++_i) {
+    //             f = _ref1[i];
+    //             _ref2 = f.tipPosition, x = _ref2[0], y = _ref2[1], z = _ref2[2];
+    //             c.fillStyle = colors[i];
+    //             _results.push(c.fillRect(x + (cWidth / 2), cHeight - y, z, z));
+    //         }
+    //         return _results;
+    //     });
+    // });
+
+
+    $scope.output = '';
+    return leap.loop(function(frame){
+        if(frame.hands.length > 0) {
+            var hand = frame.hands[0];
+            console.log('pitch: ' + hand.pitch() + ' stabilizedPalmPosition: ' + hand.stabilizedPalmPosition);
+
+            if(hand.pitch() < -.4) {
+                $scope.output = 'twist';
+                console.log('twist');
+            } else if(hand.stabilizedPalmPosition[0] < -30) {
+                $scope.output = 'pull';
+                console.log('pull');
+            } else if(Math.abs(hand.stabilizedPalmPosition[0]) < 20 && hand.stabilizedPalmPosition[2] < -20) {
+                $scope.output = 'bop';
+                console.log('bop');
+            } else if(Math.abs(hand.stabilizedPalmPosition[0]) < 30 && hand.stabilizedPalmPosition[2] > -20 && hand.stabilizedPalmPosition[2] < 50) {
+                $scope.output = 'normal';
+                console.log('normal');
             }
-            return _results;
-        });
+        }
+        return $scope.$apply();
     });
 });
