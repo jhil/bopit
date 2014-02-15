8b0eb96e97c434dc4ac627ef34859665e413d634
marielb <marielbartolome@gmail.com>
2014-02-15 13:58:51 -0500

    added counter

--- client/leap/leap.js
+++ client/leap/leap.js
@@ -29,42 +29,52 @@ bopit = angular.module('bopitApp')
     //     });
     // });
 
+    
+    var bopAudio = document.createElement('audio');
+    bopAudio.setAttribute('src', '../audio/sound-bop.mp3');
+    var pullAudio = document.createElement('audio');
+    pullAudio.setAttribute('src', '../audio/sound-pull.mp3');
+    var twistAudio = document.createElement('audio');
+    twistAudio.setAttribute('src', '../audio/sound-twist.mp3');
 
     $scope.output = '';
+    var score = 0;
     var normalFlag = false;
     return leap.loop(function(frame){
         if(frame.hands.length > 0) {
             var hand = frame.hands[0];
-            console.log('normalFlag: ' + normalFlag);
 
             if(hand.pitch() < -.4) {
-                // $scope.output = 'twist';
                 if(normalFlag){
+                    twistAudio.play();
                     $('#toyStripes').animate({ "margin-top": "-=60px" }, "fast" );
                     $('#toyStripes').animate({ "margin-top": "+=60px" }, "fast" );
+                    score = score + 1;
                 }
                 normalFlag = false;
-            } else if(hand.stabilizedPalmPosition[0] < -30) {
-                // $scope.output = 'pull';
+            } else if(hand.stabilizedPalmPosition[0] < -40) {
                 if(normalFlag){
+                    pullAudio.play();
                     $('#toyPull').animate({ "margin-left": "+=60px" }, "fast" );
                     $('#toyPull').animate({ "margin-left": "-=60px" }, "fast" );
+                    score = score + 1;
                 }
                 normalFlag = false;
             } else if(Math.abs(hand.stabilizedPalmPosition[0]) < 30 && hand.stabilizedPalmPosition[2] < -10) {
-                // $scope.output = 'bop';
                 if(normalFlag){
+                    bopAudio.play();
                     var toyBop = $('#toyBop');
                     toyBop.animate({ "margin-left": "+=35px", "margin-top": "+=10px", "height": "-=20px" }, "fast") ;
                     toyBop.animate({ "margin-left": "-=35px", "margin-top": "-=10px", "height": "+=20px" }, "fast") ;
+                    score = score + 1;
                 }
                 normalFlag = false;
             } else if(Math.abs(hand.stabilizedPalmPosition[0]) < 30 && hand.stabilizedPalmPosition[2] > -20 
                     && hand.stabilizedPalmPosition[2] < 50) {
-                // $scope.output = 'normal';
                 normalFlag = true;
             }
         }
+        $('.score h1').text(score);
         return $scope.$apply();
     });
 });
